41c843e13601e538bddbc70ed6107d77e613be39
utam0k <toru@gitpod.io>
2022-02-07 23:50:52 +0000

    dbg.

--- components/workspacekit/pkg/seccomp/notify.go
+++ components/workspacekit/pkg/seccomp/notify.go
@@ -235,9 +235,9 @@ func (h *InWorkspaceHandler) Mount(req *libseccomp.ScmpNotifReq) (val uint64, er
 		"source": source,
 		"dest":   dest,
 		"fstype": filesystem,
-	}).Debug("handling mount syscall")
+	}).Error("handling mount syscall")
 
-	if false {
+	if filesystem == "proc" || filesystem == "sysfs" {
 		// When a process wants to mount proc relative to `/proc/self` that path has no meaning outside of the processes' context.
 		// runc started doing this in https://github.com/opencontainers/runc/commit/0ca91f44f1664da834bc61115a849b56d22f595f
 		// TODO(cw): there must be a better way to handle this. Find one.
