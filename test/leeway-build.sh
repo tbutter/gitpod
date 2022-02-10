53416dfc38cbe899cf23154fdcf4758de4a2a864
Jean Pierre <jeanp413@hotmail.com>
2022-02-10 02:27:32 +0000

    :broom: cleanup

--- test/leeway-build.sh
+++ test/leeway-build.sh
@@ -5,28 +5,18 @@
 
 export CGO_ENABLED=0
 
-# Compile integration tests
-
-mkdir -p bin/integration
+mkdir -p bin
 
 for AGENT in pkg/agent/*; do
     echo building agent "$AGENT"
     base=$(basename "$AGENT")
-    go build -trimpath -ldflags="-buildid= -w -s" -o bin/integration/gitpod-integration-test-"${base%_agent}"-agent ./"$AGENT"
+    go build -trimpath -ldflags="-buildid= -w -s" -o bin/gitpod-integration-test-"${base%_agent}"-agent ./"$AGENT"
 done
 
 for COMPONENT in tests/components/*; do
     echo building test "$COMPONENT"
     OUTPUT=$(basename "$COMPONENT")
-    go test -trimpath -ldflags="-buildid= -w -s" -c -o bin/integration/"$OUTPUT".test ./"$COMPONENT"
+    go test -trimpath -ldflags="-buildid= -w -s" -c -o bin/"$OUTPUT".test ./"$COMPONENT"
 done
 
-go test -trimpath -ldflags="-buildid= -w -s" -o bin/integration/workspace.test -c ./tests/workspace
-
-# Ide integration test run separately so compile them in a different folder
-
-mkdir -p bin/ide-integration
-
-cp bin/integration/gitpod-integration-test-*-agent bin/ide-integration/
-
-go test -trimpath -ldflags="-buildid= -w -s" -o bin/ide-integration/ide.test -c ./tests/ide
+go test -trimpath -ldflags="-buildid= -w -s" -o bin/workspace -c ./tests/workspace
